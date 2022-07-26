d4c63ab6bb34b593cbfdc62e1cdd90af40c0bbe9
BlueVioletTeti <tetiana.kolomiiets.fop@gmail.com>
2022-07-26 15:29:18 +0300

    Weather forecast added

--- src/index.js
+++ src/index.js
@@ -327,6 +327,13 @@ function showTemperature(response) {
   let wind = Math.round(response.data.wind.speed);
   let placeWind = document.querySelector("#wind");
   placeWind.innerHTML = `${wind}`;
+
+  //Forecast
+  if (unitDisplayMetric === true) {
+    getForecastInC(response.data.coord);
+  } else {
+    getForecastInF(response.data.coord);
+  }
 }
 
 function determineCurrentLocationTempC(position) {
@@ -396,6 +403,7 @@ function changeCforF(event) {
   }
   let unitImperial = document.querySelector("#unit");
   unitImperial.innerHTML = `mi/h`;
+  unitDisplayMetric = false;
 }
 function changeFforC(event) {
   event.preventDefault();
@@ -406,8 +414,65 @@ function changeFforC(event) {
   }
   let unitMetric = document.querySelector("#unit");
   unitMetric.innerHTML = `m/s`;
+  unitDisplayMetric = true;
 }
 
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  return days[day];
+}
+
+function displayForecast(response) {
+  let forecast = response.data.daily;
+
+  let forecastElement = document.querySelector("#forecast");
+
+  let forecastHTML = `<div class="row">`;
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 6) {
+      forecastHTML =
+        forecastHTML +
+        `
+      <div class="col-2">
+        <div class="weather-forecast-date">${formatDay(forecastDay.dt)}</div>
+        <img
+          src="http://openweathermap.org/img/wn/${
+            forecastDay.weather[0].icon
+          }@2x.png"
+          alt=""
+          width="42"
+        />
+        <div class="weather-forecast-temperatures">
+          <span class="weather-forecast-temperature-max"> ${Math.round(
+            forecastDay.temp.max
+          )}° </span>
+          <span class="weather-forecast-temperature-min"> ${Math.round(
+            forecastDay.temp.min
+          )}° </span>
+        </div>
+      </div>
+  `;
+    }
+  });
+
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+}
+
+function getForecastInC(coordinates) {
+  let apiKey = "0774011ae8c6615118624b282251a3f1";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
+}
+
+function getForecastInF(coordinates) {
+  let apiKey = "0774011ae8c6615118624b282251a3f1";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=imperial`;
+  axios.get(apiUrl).then(displayForecast);
+}
 //Display current time
 let currentTime = new Date();
 
@@ -433,5 +498,6 @@ c.addEventListener("click", changeFforC);
 
 //Default location
 var location1 = "Kyiv";
+var unitDisplayMetric = true;
 window.currentLocationState = false;
 determineSearchLocationTempC(location1);
